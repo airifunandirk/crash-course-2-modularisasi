54b70b0dae8d42539e35fdef6137fc66b34a438c
irfan <airifunandirk@gmail.com>
2020-11-20 02:18:49 +0700

    membuat Fungsi dan Package

--- use_geometri.py
+++ use_geometri.py
@@ -3,4 +3,3 @@ from geometri.segitiga import hitung_luas_segitiga
 
 print(f"Hitung luas segitiga {hitung_luas_segitiga(10,6)}")
 print(f"Hitung luas persegi panjang {hitung_luas_persegi_panjang(10,6)}")
-
