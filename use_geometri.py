ba3d5640649400b9506fdd54f1be03796cfe75d7
irfan <airifunandirk@gmail.com>
2020-11-20 02:18:31 +0700

    membuat Fungsi dan Package

---
+++ use_geometri.py
@@ -0,0 +1,6 @@
+from geometri.persegipanjang import hitung_luas_persegi_panjang
+from geometri.segitiga import hitung_luas_segitiga
+
+print(f"Hitung luas segitiga {hitung_luas_segitiga(10,6)}")
+print(f"Hitung luas persegi panjang {hitung_luas_persegi_panjang(10,6)}")
+
